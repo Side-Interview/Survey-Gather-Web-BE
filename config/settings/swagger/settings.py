61898f13f85a663abdd2c232d1f55f797389095b
KimDuHong <art970@naver.com>
2023-10-19 04:20:59 +0900

    DOCS: api_doc Auth Setting edit

--- config/settings/swagger/settings.py
+++ config/settings/swagger/settings.py
@@ -2,12 +2,22 @@ from config.env import env
 
 SWAGGER_ENABLED = env.bool("SWAGGER_ENABLED", default=True)
 SWAGGER_SETTINGS = {
-    "SECURITY_DEFINITIONS": {
-        "JWT [Bearer {JWT}]": {
-            "name": "Authorization",
-            "type": "apiKey",
-            "in": "header",
-        }
-    },
+    # "SECURITY_DEFINITIONS": {
+    #     "JWT [Bearer {JWT}]": {
+    #         "name": "Authorization",
+    #         "type": "apiKey",
+    #         "in": "header",
+    #     }
+    # },
+    # TODO: 필요시 해당 Header 로 사용
+    #     manual_parameters=[
+    #     openapi.Parameter(
+    #         name="Authorization",
+    #         in_=openapi.IN_HEADER,
+    #         type=openapi.TYPE_STRING,
+    #         description="JWT 토큰 (Bearer {JWT})",
+    #         required=True,
+    #     ),
+    # ],
     "USE_SESSION_AUTH": False,
 }
